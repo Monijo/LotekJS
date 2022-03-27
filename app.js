66e8ecc730d347c09f142663b6848f4842d7c04f
Monijo <mokajozefowicz@gmail.com>
2022-03-27 17:50:47 +0200

    Creat Tags.

--- app.js
+++ app.js
@@ -24,3 +24,54 @@ const createTag = ({tagName="div", tagAttrs, tagText, tagId, tagEvt, tagClass}={
     }
     return node
 }
+
+const h1Ref = createTag({
+    tagName: "h1",
+    tagText: "Witaj graczu!"
+    }
+)
+const h3Ref =createTag({
+    tagName: "h3",
+    tagText: "Podaj 5(Mały lotek) lub 6(duży lotek) liczb oddzielonych przecinkiem i kliknij 'Dalej'. Rozpocznie się losowanie"
+})
+
+const inputRef = createTag({
+    tagName: "input",
+    tagAttrs: [{name: 'type', value: 'text'}],
+
+    tagId: "inputNumbers"
+
+})
+
+const buttonRef = createTag({
+    tagName: "button",
+    tagText: "Dalej",
+
+})
+
+const resultRef = createTag({
+    tagName: "p",
+    tagText: "Ilość trafień: "
+})
+
+const resultValue = createTag({
+    tagName: "span",
+})
+
+const winRef = createTag({
+    tagName: "p",
+    tagText: "Wygrałeś:"
+})
+
+const winValue = createTag({
+    tagName: "span"
+})
+
+document.body.appendChild(h1Ref)
+document.body.appendChild(h3Ref)
+document.body.appendChild(inputRef)
+document.body.appendChild(buttonRef)
+document.body.appendChild(resultRef)
+resultRef.appendChild(resultValue)
+document.body.appendChild(winRef)
+winRef.appendChild(winValue)
